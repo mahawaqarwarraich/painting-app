36a16905a2b4dde2320402b4be417142f906c6ce
Maha <maha.waqar.warraich@gmail.com>
2024-03-10 13:44:20 +0500

    remove all mouse events and add event handler for shape count

--- Form1.cs
+++ Form1.cs
@@ -23,42 +23,18 @@ namespace PaintPrac
     }
     public partial class Form1 : Form
     {
-        int X1, Y1, X2, Y2;
-        SELECTEDTOOL SelectedTool = SELECTEDTOOL.SELECT;
-        Color OutlineColor = Color.Black;
-        int OutlineSize = 2;
-        Color FillColor = Color.Transparent;
-        bool IsDrawing = false;
-        List<Shape> Shapes = new List<Shape>();
-        Triangle triangle;
-        Pentagon pentagon;
-       
-        Point TriangleVertex1;
-        Point TriangleVertex2;
-        Point TriangleVertex3;
-        Point TriangleVertex4;
-        Point TriangleVertex5;
-
-
-
-
-
-
-
-
-
-
-
 
         public Form1()
         {
             InitializeComponent();
-            this.DoubleBuffered = true;
+            
+            
         }
 
+        
         private void rightTToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SelectedTool = SELECTEDTOOL.RIGHTTRIANGLE;
+          dbDrawing.SelectedTool = SELECTEDTOOL.RIGHTTRIANGLE;
         }
 
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
@@ -68,287 +44,84 @@ namespace PaintPrac
 
         private void rectangleToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SelectedTool = SELECTEDTOOL.RECTANGLE;
+            dbDrawing.SelectedTool = SELECTEDTOOL.RECTANGLE;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+      
 
         private void circleToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SelectedTool = SELECTEDTOOL.CIRCLE;
+            dbDrawing.SelectedTool = SELECTEDTOOL.CIRCLE;
         }
 
         private void lineToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SelectedTool = SELECTEDTOOL.LINE;
+            dbDrawing.SelectedTool = SELECTEDTOOL.LINE;
         }
-        
+
         private void triangleToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            SelectedTool = SELECTEDTOOL.TRIANGLE;
-          
+            dbDrawing.SelectedTool = SELECTEDTOOL.TRIANGLE;
+
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void pentagonToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            foreach (var shape in Shapes)
-            {
-               
-                shape.Fill(e.Graphics);
-                shape.Draw(e.Graphics);
-            }
-
-            if (IsDrawing)
-            {
-              
-
-
-                switch (SelectedTool)
-                {
-                    case SELECTEDTOOL.RECTANGLE:
-
-                        /*Rectangle rect = new Rectangle(X1, Y1, X2 - X1, Y2 - Y1, OutlineColor, OutlineSize, FillColor);
-                        rect.Draw(e.Graphics);*/
-                        Pen pen = new Pen(OutlineColor, OutlineSize);
-                        e.Graphics.DrawRectangle(pen, X1, Y1, X2 - X1, Y2 - Y1);
-
-                        break;
-                    case SELECTEDTOOL.CIRCLE:
-                        Circle circle = new Circle(X1, Y1, X2 - X1, Y2 - Y1, OutlineSize, OutlineColor, FillColor);
-                        circle.Draw(e.Graphics);
-
-                        break;
-                    case SELECTEDTOOL.LINE:
-                        Line line = new Line(X1, Y1, X2, Y2, OutlineSize, OutlineColor);
-                        line.Draw(e.Graphics);
-                        break;
-                    case SELECTEDTOOL.TRIANGLE:
-                       triangle = new Triangle(TriangleVertex1, TriangleVertex2, TriangleVertex3, OutlineSize, OutlineColor, FillColor);
-                        triangle.Draw(e.Graphics);
- ;
-                        break;
-                    case SELECTEDTOOL.RIGHTTRIANGLE:
-                        triangle = new Triangle(TriangleVertex1, TriangleVertex2, TriangleVertex3, OutlineSize, OutlineColor, FillColor);
-                        triangle.Draw(e.Graphics);
-                        break;
-                    case SELECTEDTOOL.PENTAGON:
-                        pentagon = new Pentagon(TriangleVertex1, TriangleVertex2, TriangleVertex3, TriangleVertex4, TriangleVertex5, OutlineSize, OutlineColor, FillColor);
-                        pentagon.Draw(e.Graphics);
-                        break;
-                       
-                    case SELECTEDTOOL.SELECT:
-                        break;
-                    default:
-                        break;
-                }
-
-
-
-
-            }
+            dbDrawing.SelectedTool = SELECTEDTOOL.PENTAGON;
         }
 
-        private void Form1_MouseUp(object sender, MouseEventArgs e)
-        {
-           
-            IsDrawing = false;
-            Invalidate();
-
-            switch (SelectedTool)
-            {
-                case SELECTEDTOOL.RECTANGLE:
-
-                    Rectangle rect = new Rectangle(X1, Y1, X2 - X1, Y2 - Y1, OutlineColor, OutlineSize, FillColor);
-
-                    Shapes.Add(rect);
-                    break;
-                case SELECTEDTOOL.CIRCLE:
-                    Circle circle = new Circle(X1, Y1, X2 - X1, Y2 - Y1, OutlineSize, OutlineColor, FillColor);
-
-                    Shapes.Add(circle);
-                    break;
-                case SELECTEDTOOL.LINE:
-                    Line line = new Line(X1, Y1, X2, Y2, OutlineSize, OutlineColor);
-
-                    Shapes.Add(line);
-                    break;
-                case SELECTEDTOOL.TRIANGLE:
-                    Triangle triangle = new Triangle(TriangleVertex1, TriangleVertex2, TriangleVertex3, OutlineSize, OutlineColor, FillColor);
-                    Shapes.Add(triangle);
-                    break;
-
-                   
-                case SELECTEDTOOL.RIGHTTRIANGLE:
-                    triangle = new Triangle(TriangleVertex1, TriangleVertex2, TriangleVertex3, OutlineSize, OutlineColor, FillColor);
-                    Shapes.Add(triangle);
-                    break;
-                    
-                case SELECTEDTOOL.PENTAGON:
-                    pentagon = new Pentagon(TriangleVertex1, TriangleVertex2, TriangleVertex3, TriangleVertex4, TriangleVertex5, OutlineSize, OutlineColor, FillColor);
-                    Shapes.Add(pentagon);
-                    break;
-                    
-                case SELECTEDTOOL.SELECT:
-                    break;
-                default:
-                    break;
-            }
 
 
-        }
+
 
         private void pxToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ChangeOutlineSize(1);
+           dbDrawing.ChangeOutlineSize(1);
 
         }
 
         private void pxToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            ChangeOutlineSize(2);
+           dbDrawing.ChangeOutlineSize(2);
         }
 
         private void pxToolStripMenuItem2_Click(object sender, EventArgs e)
         {
-            ChangeOutlineSize(3);
+            dbDrawing.ChangeOutlineSize(3);
         }
 
         private void pxToolStripMenuItem3_Click(object sender, EventArgs e)
         {
-            ChangeOutlineSize(4);
+            dbDrawing.ChangeOutlineSize(4);
         }
 
         private void pxToolStripMenuItem4_Click(object sender, EventArgs e)
         {
-            ChangeOutlineSize(5);
+            dbDrawing.ChangeOutlineSize(5);
         }
 
-        public void ChangeOutlineSize(int size)
+      
+
+            
+        private void outlineColorToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
-            OutlineSize = size;
-            if (Shapes.Any())
-            {
-                Shape lastShape = Shapes.Last();
-                
-                lastShape.OutlineSize = size;
-                Invalidate();
-               
-            }
+            dbDrawing.ChangeOutlineColor();
+
         }
 
-        private void outlineColorToolStripMenuItem_Click(object sender, EventArgs e)
+        private void fileColorToolStripMenuItem_Click_1(object sender, EventArgs e)
         {
-            ColorDialog colorDialog = new ColorDialog();
-            if (colorDialog.ShowDialog() == DialogResult.OK)
-            {
-                if (Shapes.Any())
-                {
-                    Shape lastShape = Shapes.Last();
-
-                    lastShape.OutlineColor = colorDialog.Color;
-                    Invalidate();
-
-                }
-            }
-
+            dbDrawing.ChangeFillColor();
         }
 
-        private void fileColorToolStripMenuItem_Click(object sender, EventArgs e)
+        private void dbDrawing_Load(object sender, EventArgs e)
         {
-            ColorDialog colorDialog = new ColorDialog();
-            if (colorDialog.ShowDialog() == DialogResult.OK)
-            {
-                if (Shapes.Any())
-                {
-                    Shape lastShape = Shapes.LastOrDefault();
-
-                    lastShape.FillColor = colorDialog.Color;
-                    Invalidate();
-
-                }
-            }
+            
         }
 
-        private void pentagonToolStripMenuItem_Click(object sender, EventArgs e)
+        private void dbDrawing_NotifyShapeCount(int sCount)
         {
-            SelectedTool = SELECTEDTOOL.PENTAGON;
+            tss.Text = $"Object Count = {sCount}";
         }
-
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (IsDrawing)
-            {
-                X2 = e.X;
-                Y2 = e.Y;
-                switch (SelectedTool)
-                {
-                    case SELECTEDTOOL.TRIANGLE:
-                        
-                        TriangleVertex1 = new Point(X1 + (X2 - X1) / 2, Y1);
-                        TriangleVertex2 = new Point(X1, Y2);
-                        TriangleVertex3 = new Point(X2, Y2);
-
-                        break;
-                    case SELECTEDTOOL.RIGHTTRIANGLE:
-                        TriangleVertex1 = new Point(X1, Y1);
-                        TriangleVertex2 = new Point(X1, Y2);
-                        TriangleVertex3 = new Point(X2, Y2);
-                       
-                        break;
-                    case SELECTEDTOOL.PENTAGON:
-                        double angle = -Math.PI / 2;
-                        int radius = Math.Min(Math.Abs(X2 - X1), Math.Abs(Y2 - Y1)) / 2;
-
-                        TriangleVertex1 = new Point(
-                            (int)(X1 + (X2 - X1) / 2 + radius * Math.Cos(angle)),
-                            (int)(Y1 + radius * Math.Sin(angle))
-                        );
-
-                        angle += 2 * Math.PI / 5;
-                        TriangleVertex2 = new Point(
-                           (int)(X1 + (X2 - X1) / 2 + radius * Math.Cos(angle)),
-                           (int)(Y1 + radius * Math.Sin(angle))
-                       );
-
-                        angle += 2 * Math.PI / 5;
-                        TriangleVertex3 = new Point(
-                           (int)(X1 + (X2 - X1) / 2 + radius * Math.Cos(angle)),
-                           (int)(Y1 + radius * Math.Sin(angle))
-                       );
-
-                        angle += 2 * Math.PI / 5;
-                        TriangleVertex4 = new Point(
-                           (int)(X1 + (X2 - X1) / 2 + radius * Math.Cos(angle)),
-                           (int)(Y1 + radius * Math.Sin(angle))
-                       );
-
-                        angle += 2 * Math.PI / 5;
-                        TriangleVertex5 = new Point(
-                           (int)(X1 + (X2 - X1) / 2 + radius * Math.Cos(angle)),
-                           (int)(Y1 + radius * Math.Sin(angle))
-                       );
-
-                        break;
-
-                }
-
-                Invalidate();
-            }
-           
-
-
-        }
-
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
-        {
-            IsDrawing = true;
-            X1 = e.X;
-            Y1 = e.Y;
-
-        }
-
     }
 }
